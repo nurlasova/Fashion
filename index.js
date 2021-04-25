0a53b1b3d5dab7675b0dde8d4bbf02f1f3e0cb53
nurlasova <nurlasova.kg@gmail.com>
2021-04-25 12:40:36 +0600

    I created new project Fashion

---
+++ index.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    slidesPerView:4,
+    slidesPerGroup:1,
+      spaceBetween:25,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
